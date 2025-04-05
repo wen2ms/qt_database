d059a8c5ffaf1240f553af86f8c2f6abb850ca1d
wen2ms <1819509111@qq.com>
2025-04-06 00:51:33 +0800

    Feat: connect database

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,33 @@
 #include "mainwindow.h"
 
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QDebug>
+
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    
+    QStringList list = QSqlDatabase::drivers();
+    
+    qDebug() << list;
+    
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    
+    db.setHostName("localhost");
+    db.setUserName("root");
+    db.setDatabaseName("test");
+    // db.setPassword();
+    db.setPort(3306);
+    
+    if (!db.open()) {
+        qDebug() << "The failure: " << db.lastError().text();
+    } else {
+        qDebug() << "Connect successfully!!!";
+    }
 }
 
 MainWindow::~MainWindow() {
     delete ui;
-}
+}                                                                                                                                                    
